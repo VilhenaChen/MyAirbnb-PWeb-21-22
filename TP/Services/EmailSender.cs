3e44ea6dbb426a1d39efcd050e7d2307c5996690
VilhenaChen <diogodevilhena@sapo.pt>
2022-01-22 19:17:19 +0000

    Envio de Emails

---
+++ TP/Services/EmailSender.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Identity.UI.Services;
+using SendGrid;
+using SendGrid.Helpers.Mail;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TP.Services
+{
+    public class EmailSender : IEmailSender
+    {
+        public Task SendEmailAsync(string email, string subject, string htmlMessage)
+        {
+            var sendGridKey = @"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";
+            return Execute(sendGridKey, subject, htmlMessage, email);
+        }
+
+        public Task Execute(string apiKey, string subject, string message, string email)
+        {
+            var client = new SendGridClient(apiKey);
+            var msg = new SendGridMessage()
+            {
+                From = new EmailAddress("myairbnbtweb@protonmail.com", "MyAirbnb"),
+                Subject = subject,
+                PlainTextContent = message,
+                HtmlContent = message
+            };
+            msg.AddTo(new EmailAddress(email));
+
+            // Disable click tracking.
+            // See https://sendgrid.com/docs/User_Guide/Settings/tracking.html
+            msg.SetClickTracking(false, false);
+
+            return client.SendEmailAsync(msg);
+        }
+    }
+}
--- TP/Startup.cs
+++ TP/Startup.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.AspNetCore.Identity.UI.Services;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -13,6 +14,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using TP.Data;
 using TP.Models;
+using TP.Services;
 
 namespace TP
 {
@@ -37,6 +39,7 @@ namespace TP
                 .AddRoles<IdentityRole>()
                 .AddEntityFrameworkStores<ApplicationDbContext>();
             services.AddControllersWithViews();
+            services.AddTransient<IEmailSender, EmailSender>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
